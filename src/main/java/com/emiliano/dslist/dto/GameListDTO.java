56215ae948528e40bc85614687de5315f946040f
GustavoEmiliano <gustavoemiliano2014@gmail.com>
2025-08-24 00:58:37 -0300

    feat: endpoint implementation - get lists

---
+++ src/main/java/com/emiliano/dslist/dto/GameListDTO.java
@@ -0,0 +1,27 @@
+package com.emiliano.dslist.dto;
+
+import com.emiliano.dslist.entities.GameList;
+
+public class GameListDTO {
+
+	private Long id;
+	private String name;
+
+	public GameListDTO() {
+
+	}
+
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
