99ca632d85e03af3934ad93673ba3e27ac0163be
GustavoEmiliano <gustavoemiliano2014@gmail.com>
2025-08-24 00:43:13 -0300

    feat: get game by id

--- src/main/java/com/emiliano/dslist/controllers/GameController.java
+++ src/main/java/com/emiliano/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.emiliano.dslist.dto.GameDTO;
 import com.emiliano.dslist.dto.GameMinDTO;
 import com.emiliano.dslist.services.GameService;
 
@@ -22,5 +24,11 @@ public class GameController {
 		List<GameMinDTO> result = gameService.findAll();
 		return result;
 	}
+	 
+	 @GetMapping(value = "/{id}")  
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;
+	}  
 	
 }
